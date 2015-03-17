c2b9616be7665e7250b0f3b696f6582907c5e433
Ryan Dinesman <rdinesman@gmail.com>
2015-03-17 10:44:44 -0500

    finished contact-parser.php exercise

--- contact-parser.php
+++ contact-parser.php
@@ -5,8 +5,19 @@ function parseContacts($filename)
     $contacts = array();
 
     // todo - read file and parse contacts
-
+    $handle = fopen($filename, 'r');
+    $contents = fread($handle, filesize($filename));
+    $contactArray = explode("\n", $contents);
+    $contents = implode("|", $contactArray);
+    $contactArray = explode("|", $contents);
+    // print_r($contactArray);
+    for ($i = 0; $i < count($contactArray) -1; $i+=2){
+    	$contacts[count($contacts)] = 
+    	[
+	    	"name" => $contactArray[$i],
+	    	"number" => substr($contactArray[$i+1], 0, 3)."-".substr($contactArray[$i+1], 3, 3)."-".substr($contactArray[$i + 1], 6)
+    	];
+    }
     return $contacts;
 }
-
-var_dump(parseContacts('contacts.txt'));
+var_dump(parseContacts('data/contacts.txt'));
